b9b509ffef74e79760f6f9a561ac84ef71c439d4
ÁskerTech <victorfurquim00@gmail.com>
2023-08-01 10:32:29 -0300

    Ajeitando o card

--- src/components/Card/MainCard.jsx
+++ src/components/Card/MainCard.jsx
@@ -58,7 +58,7 @@ const MainCard = () => {
   const estaAberto = () => {
     const dataAtual = new Date();
     const horaAtual = dataAtual.getHours();
-    const horaAbertura = 6; // Replace 8 with the actual opening hour
+    const horaAbertura = 18; // Replace 8 with the actual opening hour
     const horaFechamento = 21; // Replace 22 with the actual closing hour
     return horaAtual >= horaAbertura && horaAtual <= horaFechamento;
   };
