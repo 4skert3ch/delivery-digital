27889f31b4ae3e20188a9c43a5c0a482d85d1789
ÁskerTech <victorfurquim00@gmail.com>
2023-08-08 08:34:25 -0300

    ASKER-TECH.COM

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -22,10 +22,10 @@ const Navbar = ({ totalAPagar, setTotalAPagar, setCarrinho, isAberto }) => {
             </strong>
           </span>
 
-          <a href="#/" className='col-12'>
+          <a href="https://askertech-1d73c38e21b5.herokuapp.com" className='col-12'>
             <div className="contador">
               <label id="backContador">Total a pagar<p id="contador" className="form-control">R${(totalAPagar / 100).toFixed(2)}</p></label>
-              <div href="https://www.google.com" class="floating-label">ÁSKER-TECH</div>
+              <div class="floating-label">ÁSKER-TECH.COM</div>
             </div>
           </a>
           <button type="button" className="btn btn-success col-6" data-bs-toggle="modal" data-bs-target="#modalConfirmar" id="btnRequest" disabled={valorZerado}>
