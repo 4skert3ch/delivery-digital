a02b63bd55c9c8268f706eae31aa4d87ae908047
Lucas Longo <lsama@protonmail.com>
2023-09-02 12:22:20 -0300

    Cardápio Super Vale

--- src/components/Card/Produtos.jsx
+++ src/components/Card/Produtos.jsx
@@ -1,89 +1,30 @@
 const produtos = [
-    {
-      nome: "PORÇÃO / Meia",
-      descricao: "Alcatra, frango, calabresa, fritas, farofa, pepino e pão com queijo",
-      preco: 7600,
-      imagemUrl: "https://th.bing.com/th/id/OIP.ade_zws1Q_aHdK3qYCCdFgHaE8?pid=ImgDet&rs=1"
-    },
-    {
-      nome: "X-Calabresa",
-      descricao: "Calabresa, hambúger, queijo, alface, tomate, pepino, milho e ervilha",
-      preco: 2500,
-      imagemUrl: "https://th.bing.com/th/id/OIP.1WLeEc6y7L4qeADu4jEFvgHaE8?pid=ImgDet&rs=1"
-    },
-    {
-      nome: "X-Frango",
-      descricao: "Frango, queijo, alface, tomate, pepino, milho e ervilha",
-      preco: 2399,
-      imagemUrl: "https://th.bing.com/th/id/OIP.ym3M4ybCDW1pYpPmGHsN8wHaEj?pid=ImgDet&rs=1"
-    },
-    {
-      nome: "X-Bacon",
-      descricao: "Hambúrguer, bacon, queijo, preseunto, alface, tomate, pepino, milho e ervilha",
-      preco: 2600,
-      imagemUrl: "https://th.bing.com/th/id/OIP.AvI5N2smm4X9PxxJjGPGJgHaEn?pid=ImgDet&rs=1"
-    },
-    {
-      nome: "X-Alcatra",
-      descricao: "Alcatra, queijo, alface, tomate, pepino, milho e ervilha",
-      preco: 2900,
-      imagemUrl: "https://th.bing.com/th/id/R.e95e4719720dce518b8628ac2bfc3348?rik=YXTOigeMXwKDxA&riu=http%3a%2f%2fblog.tnh1.com.br%2fnidelins%2fwp-content%2fuploads%2f2014%2f08%2fmontagu%c2%b4s5.jpg&ehk=SITnAVl0C%2bcDi0KFpw1BmmGTz5cdYruO%2fpanIvMzjGE%3d&risl=&pid=ImgRaw&r=0"
-    },
-    {
-      nome: "PORÇÃO / Inteira",
-      descricao: "PORÇÃO PÉROLAS/ Inteira",
-      preco: 9800,
-      imagemUrl: "https://th.bing.com/th/id/OIP.wSYDezrpVMgaw-oM5nHxMwHaDz?pid=ImgDet&rs=1"
-    },
-    {
-      nome: "BATATA FRITA / Meia",
-      preco: 2100,
-      imagemUrl: "https://i.ibb.co/znQXv6Q/batatameia.png"
-    },
-    {
-      nome: "BATATA FRITA / Inteira",
-      preco: 3300,
-      imagemUrl: "https://th.bing.com/th/id/OIP.LkjpcBTtSb91JgHvuNolLAHaE7?pid=ImgDet&rs=1"
-    },
-    {
-      nome: "Coca-Cola 1,5L",
-      preco: 1200,
-      imagemUrl: "https://www.trinkgut.de/media/image/7d/1c/3b/5000112547726.png"
-    },
-    {
-      nome: "Coca-Cola 600ml",
-      preco: 800,
-      imagemUrl: "https://n4.sdlcdn.com/imgs/d/7/e/Coca-Cola-Cola-600-ml-SDL967588379-1-c06f5.jpg"
-    },
-    {
-      nome: "Coca-Cola 350ml",
-      preco: 600,
-      imagemUrl: "https://cdn.dooca.store/418/products/coca.jpg?v=1589835707&webp=0"
-    },
-    {
-      nome: "Guaraná 1,5L",
-      preco: 2399,
-      imagemUrl: "https://th.bing.com/th/id/OIP.bHsGG_SsCh4I0TpJHQOEVwHaHa?pid=ImgDet&rs=1"
-    },
-    {
-      nome: "Guaraná 600ml",
-      preco: 800,
-      imagemUrl: "https://apoioentrega.vteximg.com.br/arquivos/ids/459548-1000-1000/def63cee85539b67479205ee45835997_refrigerante-antarctica-guarana-garrafa-600-ml---refrig-antarctica-600ml-pet-guarana---1-un_lett_2.jpg?v=637305879246200000"
-    },
-    {
-      nome: "Guaraná 350ml",
-      descricao: "Description of the product goes here.",
-      preco: 600,
-      imagemUrl: "https://apoioentrega.vteximg.com.br/arquivos/ids/459546-1000-1000/52f217b9ecacfa07b163c321348257e0_refrigerante-guarana-antarctica-lata-350ml---refrig-antarctica-350ml-lt-guarana---1-un_lett_2.jpg?v=637305879165000000"
-    },
-    {
-      nome: "Água mineral",
-      descricao: "Description of the product goes here.",
-      preco: 400,
-      imagemUrl: "https://th.bing.com/th/id/OIP.6CDUqpv9Hk8e3_5s9ImjUQHaNn?pid=ImgDet&rs=1"
-    },
-    // Adicione caminhos relativos para as imagens dos demais produtos conforme necessário
-  ];
+  {
+    nome: "Skol 269 ml",
+    descricao: "Cerveja Skol lata 269 ml",
+    preco: 0,
+    imagemUrl: "https://io.convertiez.com.br/m/trimais/shop/products/images/2753/large/cerveja-skol-269-ml_2731.jpg"
+  },
+  {
+    nome: "Heineken garrafa",
+    descricao: "Cerveja Heineken garrafa 600 ml",
+    preco: 0,
+    imagemUrl: "https://statics.angeloni.com.br/super/files/produtos/2302038/2302038_2_large.jpg"
+  },
+  {
+    nome: "Brahma 350 ml",
+    descricao: "Cerveja Brahma lata 350 ml",
+    preco: 0,
+    imagemUrl: "https://choppbrahmaexpress.vtexassets.com/arquivos/ids/155702/brahma-lata-350ml.jpg?v=637353454674430000"
+  },
+  {
+    nome: "Brahma 473 ml",
+    descricao: "Cerveja Brahma lata 473 ml",
+    preco: 0,
+    imagemUrl: "https://superirmao.loji.com.br/storage/uploads/uiyFFuF1ELli4NoMv1rhFpnJSN5ZWTmrQBTSDGMa.jpeg"
+  }
+  // Adicione caminhos relativos para as imagens dos demais produtos conforme necessário
+];
 
-  
-  export default produtos;
+
+export default produtos;
